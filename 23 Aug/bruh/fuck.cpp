2549318e9575b88fb896f605ea979d42dadde2c6
Jamie R <jami3507@gmail.com>
2023-08-23 16:52:54 -0500

    i hate it here

---
+++ 23 Aug/bruh/fuck.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    unsigned int posnum = 435;
+    float realnum = 468.16;
+
+    cout << fixed;
+    cout << realnum << endl;
+}
